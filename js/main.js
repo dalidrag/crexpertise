c8b0575ae2a4e545bc6ef928b1efdbc14ee0fb0a
Dalibor Dragojevic <daco.dragojevic@gmail.com>
2018-01-28 13:51:36 +0100

    - Responsive positioning of hotspots added - Snapping buttons dynamically added from JS based on sections definition array

--- js/main.js
+++ js/main.js
@@ -1,33 +1,49 @@
 window.addEventListener('scroll', ProcessScroll, false);
-window.addEventListener('click', SnapToSection, false);
-
 
 ProcessScroll.frontMountain = document.querySelector('.parallax-layer.front');
 ProcessScroll.backMountain = document.querySelector('.parallax-layer.back');
 
-ProcessScroll.SnapButtons = document.getElementsByClassName('snap-button');
-ProcessScroll.SnapButtons[0].style.backgroundColor = 'yellow';
-ProcessScroll.previousSnapButton = ProcessScroll.SnapButtons[0];
 ProcessScroll.previousSection = 0;
 var parallaxContainer = document.getElementsByClassName('parallax')[0];
 ProcessScroll.parallaxContainerCoords = parallaxContainer.getBoundingClientRect();
 
 GetSection.sections = [500, 900, 1500, 2200, 2800, 3500];
 
-var hotSpots = [{x: 900, y: 100, width: 300}, {x: 600, y: 900, width: 400}, {x: 500, y: 1300, width: 500}];
+// x and y are in the percentage of the screen width, relative to mountain top
+// width is in pixels
+var hotSpots = [{x: 48, y: 4.5, width: 300}, {x: 39, y: 52, width: 400}, {x: 33.5, y: 89, width: 500}];
 
+// Initialize hotspots
 var hotSpot = null;
 for (var i = 0; i < hotSpots.length; ++i) {
+
     hotSpot = document.createElement('div');
     hotSpot.classList.add('hot-spot');
-    hotSpot.style.left = hotSpots[i].x + 'px';
-    hotSpot.style.top = hotSpots[i].y + 'px';
+    hotSpot.style.left = hotSpots[i].x + 'vw';
+    hotSpot.style.top = hotSpots[i].y + 'vw';
 
     parallaxContainer.appendChild(hotSpot);
 }
 
+// Initialize snapping buttons
+var snappingButton = null;
+for (i = 0; i < GetSection.sections.length; ++i ) {
+
+    snappingButton = document.createElement('div');
+    snappingButton.classList.add('snap-button');
+    snappingButton.classList.add('btn' + i);
+    snappingButton.addEventListener('click', SnapToSection, false);
+
+    document.getElementById('snap-widget').appendChild(snappingButton);
+
+}
+ProcessScroll.SnapButtons = document.getElementsByClassName('snap-button');
+ProcessScroll.SnapButtons[0].style.backgroundColor = 'yellow';
+ProcessScroll.previousSnapButton = ProcessScroll.SnapButtons[0];
+
+
 function GetSection(yPos) {
-    var i, j = 0;
+    var i, j;
 
     for ( i = 0; i < GetSection.sections.length; ++i ) {
         if (yPos <= GetSection.sections[i]) break;
@@ -67,7 +83,8 @@ function ProcessScroll() {
 
 
     for (var i = 0; i < hotSpots.length; ++i) {
-        if ( (pageYOffset + innerHeight/2 + 100) > hotSpots[i].y + ProcessScroll.parallaxContainerCoords.top )
+
+        if ( (pageYOffset + innerHeight/2 + 100) > (hotSpots[i].y * innerWidth / 100) + ProcessScroll.parallaxContainerCoords.top )
             RevealHotSpot(document.getElementsByClassName('hot-spot')[i], hotSpots[i].width);
 
     }
@@ -84,8 +101,10 @@ function RevealHotSpot(el, targetWidth) {
 
 
 function SnapToSection(evt) {
+    evt.stopPropagation();
     var btnClassName = evt.target.classList[1];
 
+    // TODO for Chrome document.documentElement => document.body
     ScrollTo(document.documentElement, GetSection.sections[btnClassName[3]], 800);
 }
 
@@ -97,8 +116,7 @@ function ScrollTo(element, to, duration) {
 
     var animateScroll = function(){
         currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
-        element.scrollTop = val;
+        element.scrollTop = Math.easeInOutQuad(currentTime, start, change, duration);
         if(currentTime < duration) {
             setTimeout(animateScroll, increment);
         }
